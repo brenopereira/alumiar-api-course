786347a28d85d2cf964c1953f734c7b0832bd186
Breno Pereira <brenomartinsxd@gmail.com>
2019-10-30 00:04:22 -0400

    Add changes into post schema

--- database/migrations/1572408102629_post_schema.js
+++ database/migrations/1572408102629_post_schema.js
@@ -1,19 +1,26 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class PostSchema extends Schema {
-  up () {
-    this.create('posts', (table) => {
-      table.increments()
-      table.timestamps()
-    })
+  up() {
+    this.create("posts", table => {
+      table.increments();
+      table.string("title");
+      table.string("content");
+      table
+        .integer("category_id")
+        .unsigned()
+        .references("id")
+        .inTable("categories");
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('posts')
+  down() {
+    this.drop("posts");
   }
 }
 
-module.exports = PostSchema
+module.exports = PostSchema;
